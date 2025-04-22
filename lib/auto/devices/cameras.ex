48a769ed2bc54a6bb0bf3ecc5ee7df5900953122
Lars Wikman <lars@underjord.io>
2025-04-22 11:15:58 +0200

    Add a check to avoid flashing lights at me when camera is accessed

--- lib/auto/devices/cameras.ex
+++ lib/auto/devices/cameras.ex
@@ -2,13 +2,14 @@ defmodule Auto.Devices.Cameras do
   use GenServer
 
   @check_interval 1000
+  @times 2
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, opts)
   end
 
   def init(opts) do
     send(self(), :check_cameras)
-    {:ok, %{in_use?: false}}
+    {:ok, %{in_use?: false, times: 0}}
   end
 
   def handle_info(:check_cameras, state) do
@@ -31,11 +32,18 @@ defmodule Auto.Devices.Cameras do
                |> String.ends_with?("0"))
       end
 
-    case {state.in_use?, in_use?} |> IO.inspect(label: "camera change?") do
-      {true, false} ->
+    times =
+      case {in_use?, state.in_use?} do
+        {false, _} -> 0
+        {true, false} -> 1
+        {true, true} -> state.times + 1
+      end
+
+    case {state.in_use?, in_use?, times} |> IO.inspect(label: "camera change?") do
+      {true, false, _} ->
         Phoenix.PubSub.broadcast(Auto.PubSub, "cameras", :cameras_stopped)
 
-      {false, true} ->
+      {_, true, times} when times >= @times ->
         Phoenix.PubSub.broadcast(Auto.PubSub, "cameras", :cameras_started)
 
       _ ->
@@ -43,7 +51,7 @@ defmodule Auto.Devices.Cameras do
     end
 
     Process.send_after(self(), :check_cameras, @check_interval)
-    {:noreply, %{state | in_use?: in_use?}}
+    {:noreply, %{state | in_use?: in_use?, times: times}}
   end
 
   def handle_info(_, state) do
